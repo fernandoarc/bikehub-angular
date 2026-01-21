5b65136e225b3cd3691f6dd6ea64e1196c7c0914
Fernando Raihuanque <fernando.arc02@gmail.com>
2026-01-21 14:44:38 -0300

    feat(BKH-008): add HTTP interceptor for headers and error logging

---
+++ src/core/http/http-error.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpInterceptorFn, HttpErrorResponse } from "@angular/common/http";
+import { catchError, throwError } from "rxjs";
+
+export const httpErrorInterceptor: HttpInterceptorFn = (req, next) => {
+    const request = req.clone({
+        setHeaders: {
+            'X-App-Name': 'bikehub-angular'
+        },
+    });
+    return next(request).pipe(
+        catchError((err: unknown) => {
+            if(err instanceof HttpErrorResponse) {
+                console.error('[HTTP ERROR]:', {
+                    url: err.url,
+                    status: err.status,
+                    message: err.message,
+                });
+            } else {
+                console.error('[HTTP ERROR] Unknown error occurred:', err);
+            }
+            return throwError(() => err);
+        }),
+    );
+};
+
